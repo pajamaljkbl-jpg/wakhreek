8e28a3cadf9767dfcda1bb2e40df539275090f4a
pajamaljkbl-jpg <pajamaljkbl@gmail.com>
2026-09-07 22:24:04 +0000

    Correct import statement in layout.js
    Fix import statement formatting and clean up layout.js.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
-<link rel="manifest" href="/manifest.json" />import './globals.css'
-
+import './globals.css'
 export const metadata = {
   title: 'WakhReek -Onley Took',
   description: 'WakhReek -Onley Took',
