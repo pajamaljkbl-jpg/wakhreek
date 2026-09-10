807a963425cf0726b96954891e043149434ddbb3
pajamaljkbl-jpg <pajamaljkbl@gmail.com>
2026-09-10 23:15:15 +0000

    Enable seven languages across WakhReek

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import './globals.css'
 import PushRegistrar from './PushRegistrar'
 import CommunicationTvBand from '../components/CommunicationTvBand'
 import OutgoingCallTone from '../components/OutgoingCallTone'
+import LanguageSwitcher from '../components/LanguageSwitcher'
+import AutoTranslator from '../components/AutoTranslator'
 
 export const metadata = {
   title: "WakhReek",
@@ -12,12 +14,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body>
         <PushRegistrar />
         {children}
         <CommunicationTvBand />
         <OutgoingCallTone />
+        <AutoTranslator />
+        <LanguageSwitcher />
       </body>
     </html>
   )
