0f12968ed992fb00386176fba93907b1097369b7
pajamaljkbl-jpg <pajamaljkbl@gmail.com>
2026-09-12 21:56:31 +0000

    Mount central i18n provider safely

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import OutgoingCallTone from '../components/OutgoingCallTone'
 import LanguageSwitcher from '../components/LanguageSwitcher'
 import PlatformTranslator from '../components/PlatformTranslator'
 import LanguageProofreader from '../components/LanguageProofreader'
+import I18nProvider from '../components/I18nProvider'
 
 export const metadata = {
   title: "WakhReek",
@@ -17,13 +18,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body>
-        <PushRegistrar />
-        {children}
-        <CommunicationTvBand />
-        <OutgoingCallTone />
-        <PlatformTranslator />
-        <LanguageProofreader />
-        <LanguageSwitcher />
+        <I18nProvider>
+          <PushRegistrar />
+          {children}
+          <CommunicationTvBand />
+          <OutgoingCallTone />
+          <PlatformTranslator />
+          <LanguageProofreader />
+          <LanguageSwitcher />
+        </I18nProvider>
       </body>
     </html>
   )
