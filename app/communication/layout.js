71f3058de06cf2e4b8cc482337491a8f2655a632
pajamaljkbl-jpg <pajamaljkbl@gmail.com>
2026-09-12 09:59:13 +0000

    Enable TURN bootstrap for Communication

---
+++ app/communication/layout.js
@@ -0,0 +1,10 @@
+import TurnBootstrap from './TurnBootstrap'
+
+export default function CommunicationLayout({ children }) {
+  return (
+    <>
+      <TurnBootstrap />
+      {children}
+    </>
+  )
+}
