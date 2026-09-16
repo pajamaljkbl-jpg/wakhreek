204fd3a7f95221c9cfb907586a7bd1d0a6bf7053
pajamaljkbl-jpg <pajamaljkbl@gmail.com>
2026-09-16 11:50:27 +0000

    Mount admin shortcut for admin sessions

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import I18nProvider from '../components/I18nProvider'
 import AnalyticsTracker from '../components/AnalyticsTracker'
 import NewUserAssistant from '../components/NewUserAssistant'
 import BoutiqueOwnerAssistant from '../components/BoutiqueOwnerAssistant'
+import AdminShortcut from '../components/AdminShortcut'
 
 export const metadata = {
   title: "WakhReek",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
           <AnalyticsTracker />
           <PushRegistrar />
           {children}
+          <AdminShortcut />
           <NewUserAssistant />
           <BoutiqueOwnerAssistant />
           <CommunicationTvBand />
